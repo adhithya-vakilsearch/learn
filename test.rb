5f533ec037e4d27221c1b828449eec8778a87101
adhithya <adhithya@vakilsearch.com>
2015-09-02 17:53:04 +0530

    upto stings

--- test.rb
+++ test.rb
@@ -14,13 +14,9 @@ puts 1231313+311414-234
 
 10.times do print "Hello, world!" end
 
-
-
-
 puts print "adhi,adhithya"
 
 puts 10./3
-
 
 puts 10.0/3
 
@@ -28,5 +24,48 @@ x = 1025
 
 puts x * 25
 
+Kernel.puts "Hello world!"
+
+puts "This is a test".length
+
+puts "Test".downcase
+puts "Test" + "Test"
+
+def dog_barking
+puts "Woooooof!"
+end
+dog_barking
+
+a=500
+b=200
+
+puts b-a
+
+a=10
+b=50
+c=a+b
+
+puts c
 
 
+age = 25
+puts "You're 24!" if age == 24
+
+2.times do puts "adhi" end
+
+	puts "Hello".class
+
+
+x = "Test"
+y = "String"
+puts "Success!" if x + y == "TestString"
+
+
+
+puts "b" > "a"
+
+puts "b" > "r"
+
+
+
+puts "It's a #{"nice " * 5}world"
