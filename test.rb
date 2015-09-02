9f3cf5f131ba784ec90306436868e6f5979296c8
adhithya <adhithya@vakilsearch.com>
2015-09-02 15:47:18 +0530

    basic ruby cmds

---
+++ test.rb
@@ -0,0 +1,25 @@
+
+
+puts "Hello World"
+
+puts Time.now
+
+puts Time.now.strftime("Printed on %m/%d/%Y") 
+
+puts Time.now.strftime("%A , %d-%b-%Y")
+
+puts 1+1
+
+puts 1231313+311414-234
+
+10.times do print "Hello, world!" end
+
+
+
+
+puts print "adhi,adhithya"
+
+puts 10./3
+
+
+puts 10.0/3
