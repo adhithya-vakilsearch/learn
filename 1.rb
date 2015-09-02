74fbfac2d62682f0c34e5f1284100cc0e145db35
adhithya <adhithya@vakilsearch.com>
2015-09-02 15:39:58 +0530

    basic cmds

--- 1.rb
+++ 1.rb
@@ -7,3 +7,9 @@ puts Time.now
 puts Time.now.strftime("Printed on %m/%d/%Y") 
 
 puts Time.now.strftime("%A , %d-%b-%Y")
+
+puts 1+1
+
+puts 1231313+311414-234
+
+10.times do print "Hello, world!" end
